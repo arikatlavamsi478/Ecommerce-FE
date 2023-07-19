efc730ad8ee15888c1cfff0745faaf6fcca3a33f
arikatlavamsi478 <arikatlavamsi478@users.noreply.github.com>
2023-07-18 22:19:36 -0700

    3 files modified, 2 added --> Added registarrtion form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
+import RegistrationForm from './components/RegistrationForm';
 import './style.css';
 
 export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+  return <RegistrationForm />;
 }
